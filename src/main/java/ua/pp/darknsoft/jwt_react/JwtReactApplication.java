fb6751c541bd188991a17fa1602912c981516d2f
andrewdark <samsonov.a@ukr.net>
2024-09-21 17:07:26 +0300

    add springboot carcass

---
+++ src/main/java/ua/pp/darknsoft/jwt_react/JwtReactApplication.java
@@ -0,0 +1,13 @@
+package ua.pp.darknsoft.jwt_react;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtReactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtReactApplication.class, args);
+	}
+
+}
