18c20b7b26d7e77042aa8122b3c20edecc11a856
andrewdark <samsonov.a@ukr.net>
2024-11-09 23:27:45 +0200

    HotFix

--- src/main/java/ua/pp/darknsoft/jwt/repositories/AppRefreshTokenRepository.java
+++ src/main/java/ua/pp/darknsoft/jwt/repositories/AppRefreshTokenRepository.java
@@ -2,6 +2,10 @@ package ua.pp.darknsoft.jwt.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import ua.pp.darknsoft.jwt.models.AppRefreshToken;
+import ua.pp.darknsoft.jwt.models.AppUser;
+
+import java.util.Optional;
 
 public interface AppRefreshTokenRepository extends JpaRepository<AppRefreshToken, Long> {
+    Optional<AppRefreshToken> findAppRefreshTokenByAppUser(AppUser appUser);
 }
--- src/main/java/ua/pp/darknsoft/jwt/services/AppRefreshTokenServiceImpl.java
+++ src/main/java/ua/pp/darknsoft/jwt/services/AppRefreshTokenServiceImpl.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ua.pp.darknsoft.jwt.dto.RefreshTokenDTO;
 import ua.pp.darknsoft.jwt.models.AppRefreshToken;
+import ua.pp.darknsoft.jwt.models.AppUser;
 import ua.pp.darknsoft.jwt.repositories.AppRefreshTokenRepository;
 
 @Service
@@ -17,6 +18,13 @@ public class AppRefreshTokenServiceImpl implements AppRefreshTokenService{
 
     @Override
     public AppRefreshToken save(AppRefreshToken dto) {
+        AppUser appUser = dto.getAppUser();
+        if( appRefreshTokenRepository.findAppRefreshTokenByAppUser(appUser).isPresent()){
+            AppRefreshToken persistent = appRefreshTokenRepository.findAppRefreshTokenByAppUser(appUser).get();
+            persistent.setRefreshToken(dto.getRefreshToken());
+            return appRefreshTokenRepository.save(persistent);
+        }
+
         return appRefreshTokenRepository.save(dto);
     }
 }
