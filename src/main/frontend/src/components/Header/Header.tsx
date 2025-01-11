ef6f22b54c3c61a797914bc5ce7e1f0573792549
andrewdark <samsonov.a@ukr.net>
2025-01-11 22:25:54 +0200

    add react icon lib

--- src/main/frontend/src/components/Header/Header.tsx
+++ src/main/frontend/src/components/Header/Header.tsx
@@ -1,11 +1,12 @@
 import css from "./Header.module.css";
+import { HiUser } from "react-icons/hi";
 
 export const Header = () => {
     return (
         <div className={css.container}>
             <header className={css.header}>
                 <div>Logo</div>
-                <div>User</div>
+                <div><HiUser className="my-icon" size="24" />User</div>
             </header>
         </div>
     );
